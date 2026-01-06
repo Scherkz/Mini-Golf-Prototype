b6fe71caaa586a7a31fb74a4dc70e29e3c81e364
Owndbyfreak <ma.wohlfarth@gmx.de>
2026-01-06 11:30:08 +0100

    Changes in SplitBallPowerUp.cs

--- Assets/BuildSystem/Buildings/SplitBallPowerUp/SplitBallPowerUp.cs
+++ Assets/BuildSystem/Buildings/SplitBallPowerUp/SplitBallPowerUp.cs
@@ -20,24 +20,21 @@ public class SplitBallPowerUp : PowerUpBuilding
         clone.transform.position = originalBall.transform.position + (Vector3)cloneSpawnOffset;
         clone.transform.rotation = originalBall.transform.rotation;
         
-        var cloneController = clone.GetComponent<PlayerController>();
-        if (cloneController != null)
-        {
-            cloneController.SetResetOnStart(false);          
-            cloneController.SetColor(player.GetColor());     
-        }
         
+        var cloneController = clone.GetComponent<PlayerController>();
+        cloneController.SetResetOnStart(false);          
+        cloneController.SetColor(player.GetColor());     
+            
         var cloneRb = clone.GetComponent<Rigidbody2D>();
         if (cloneRb != null)
         {
-            var speed =snapLinearVel.magnitude;
+            var speed = snapLinearVel.magnitude;
             var dir = snapLinearVel / speed;
 
-            var angle = Random.value < 0.5f ? -spreadDegrees : spreadDegrees;
-            var spreadDir = (Vector2)(Quaternion.Euler(0f, 0f, angle) * dir);
+            var spreadDir = (Vector2)(Quaternion.Euler(0f, 0f, spreadDegrees) * dir);
 
             var perp = new Vector2(-dir.y, dir.x);
-            var side = Mathf.Sign(angle);
+            var side = Mathf.Sign(spreadDegrees);
             clone.transform.position = originalBall.transform.position + (Vector3)(perp * cloneSpawnOffset * side);
 
             cloneRb.linearVelocity = spreadDir * speed;
