c816ba708aa3a3358e2da03e52cd1b799c260a18
Scherkz <96747456+Scherkz@users.noreply.github.com>
2026-01-19 02:57:43 +0100

    Add gravity Animation to GravityField

---
+++ Assets/World/GravityField/GravityPulse.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GravityPulse : MonoBehaviour
+{
+    private float maxRadius;
+    private float currentRadius;
+    private float spriteBaseSize;
+    private float secondsPerAnimationCycle;
+    private float animationSpeed;
+
+    private void Start()
+    {
+        CircleCollider2D parentCollider = GetComponentInParent<CircleCollider2D>();
+        maxRadius = parentCollider.radius;
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        spriteBaseSize = sr.sprite.bounds.size.x;
+
+        secondsPerAnimationCycle = 4f;
+        animationSpeed = maxRadius / secondsPerAnimationCycle;
+        //currentRadius = maxRadius;
+    }
+
+    private void Update()
+    {
+        currentRadius -= animationSpeed * Time.deltaTime;
+        if (currentRadius < 0)
+        {
+            currentRadius = maxRadius;
+        }
+        float desiredDiameter = currentRadius * 2f;
+        float scale = desiredDiameter / spriteBaseSize;
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
