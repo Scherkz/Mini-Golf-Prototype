aee0628365e89a6a6677076e33b8650a69ca073c
Scherkz <96747456+Scherkz@users.noreply.github.com>
2026-01-20 22:58:24 +0100

    remove debug message

--- Assets/BuildSystem/Buildings/Booster/BoosterBuilding.cs
+++ Assets/BuildSystem/Buildings/Booster/BoosterBuilding.cs
@@ -4,10 +4,8 @@ public class BoosterBuilding : Building
 {
     public float boostForce;
 
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.attachedRigidbody.AddForce(rotationAnchor.transform.right * boostForce, ForceMode2D.Impulse);
-        Debug.Log("Boost!");
     }
 }
