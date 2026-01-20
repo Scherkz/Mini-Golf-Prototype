0cdd58553bb3641e2537d9d8db2fd0d23dffe326
JostMK <jost.klink@gmail.com>
2026-01-20 20:45:22 +0100

    Create gravity rings in shader
    This makes the rings constant size while shrinking.

--- Assets/World/GravityField/GravityField.cs
+++ Assets/World/GravityField/GravityField.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class GravityField : MonoBehaviour
 {
+    private const string TimeOffsetShaderPropertyName = "_TimeOffset";
+
     public float gravityFactor = 9.81f;
+
+    private void Awake()
+    {
+        var radius = GetComponent<CircleCollider2D>().radius;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            if (child.name.StartsWith("Visualizer"))
+            {
+                child.transform.localScale = new Vector3(radius, radius, 1);
+
+                var propBlock = new MaterialPropertyBlock();
+                propBlock.SetFloat(TimeOffsetShaderPropertyName, Random.value);
+                child.GetComponent<SpriteRenderer>().SetPropertyBlock(propBlock);
+            }
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
