8e3e8f3fc83d2b12ebf0a5c530844661c98aedfd
Scherkz <96747456+Scherkz@users.noreply.github.com>
2026-01-24 13:29:35 +0100

    Add shoot logic

--- Assets/BuildSystem/Buildings/Cannon/CannonBuilding.cs
+++ Assets/BuildSystem/Buildings/Cannon/CannonBuilding.cs
@@ -1,5 +1,102 @@
 using UnityEngine;
+using System.Collections;
+using Unity.VisualScripting;
+
 
 public class CannonBuilding : Building
 {
+    private bool isFree;
+    private Coroutine shootCoroutine;
+    private float shootPower = 15f;
+    private Collider2D myCollider;
+    private Collider2D otherCollider;
+    private GameObject playerGameObject;
+
+    private void Awake()
+    {
+        isFree = true;
+        myCollider = GetComponentInChildren<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) return;
+
+        if (isFree)
+        {
+            playerGameObject = collision.gameObject;
+            isFree = false;
+            otherCollider = collision.collider;
+            Physics2D.IgnoreCollision(myCollider, otherCollider, true);
+            FreezePlayer();
+            shootCoroutine = StartCoroutine(ShootAfterDelay());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (shootCoroutine != null)
+                StopCoroutine(shootCoroutine);
+        }
+    }
+
+    IEnumerator ShootAfterDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        DefreezePlayer();
+        Rigidbody2D rb = playerGameObject.GetComponent<Rigidbody2D>();
+        Vector2 dir = new Vector2(1f, 1f).normalized;
+        rb.linearVelocity = dir * shootPower;
+    }
+
+    private void FreezePlayer()
+    {
+        Rigidbody2D rb = playerGameObject.GetComponent<Rigidbody2D>();
+
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+
+        foreach (Collider c in playerGameObject.GetComponentsInChildren<Collider>())
+            c.enabled = false;
+
+        Vector2 midPoint = transform.position;
+        midPoint.x += 0.5f;
+        midPoint.y += 0.5f;
+        playerGameObject.transform.position = midPoint;
+    }
+
+    private void DefreezePlayer()
+    {
+        Rigidbody2D rb = playerGameObject.GetComponent<Rigidbody2D>();
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.linearVelocity = Vector2.zero;
+        rb.angularVelocity = 0f;
+
+        foreach (Collider c in playerGameObject.GetComponentsInChildren<Collider>())
+            c.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (isFree) return;
+
+        Vector2 posA = transform.position;
+        Vector2 posB = playerGameObject.transform.position;
+        float distance = Vector2.Distance(posA, posB);
+        if (distance > 2f)
+        {
+            Physics2D.IgnoreCollision(myCollider, otherCollider, false);
+            isFree = true;
+            playerGameObject = null;
+        }
+    }
 }
