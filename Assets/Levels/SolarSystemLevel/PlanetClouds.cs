de0e098927d8770d4a0b9666cbb185a80def8cee
JostMK <jost.klink@gmail.com>
2026-01-27 16:37:54 +0100

    Fix clouds in solar system map

--- Assets/Levels/SolarSystemLevel/PlanetClouds.cs
+++ Assets/Levels/SolarSystemLevel/PlanetClouds.cs
@@ -5,17 +5,19 @@ public class PlanetClouds : MonoBehaviour
     [SerializeField] private float scrollSpeed = 0.5f;
 
     private float loopOffset;
+    private float startOffset;
 
     private void Awake()
     {
         var spriteRenderer = GetComponent<SpriteRenderer>();
         loopOffset = spriteRenderer.size.x * 0.5f;
+        startOffset = loopOffset * Random.value;
     }
 
     private void Update()
     {
-        float xOffset = Mathf.Repeat(Time.time * scrollSpeed, loopOffset);
+        float xOffset = Mathf.Repeat(Time.time * scrollSpeed + startOffset, loopOffset);
         float x = -(loopOffset * 0.5f) + xOffset;
-        transform.position = new Vector3(x, 0, 0);
+        transform.localPosition = new Vector3(x, 0, 0);
     }
 }
